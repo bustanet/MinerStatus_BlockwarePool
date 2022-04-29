9ba2ffb22fe40804aa1086e918a8bcbf738bf452
Anthony Bustamante <anthony.bustamante@gmail.com>
2022-04-29 13:27:29 -0700

    Complete refactor of code
    - I was making an API call for each individual miner check. Instead of just calling the API once and doing all the calculations server side.
    - Added additional debugging logs.
    - Original logic did not check for offline/inactive miners. New logic accounts for this.
    - Moved try/except statements closer to the code that was expected to throw an error, instead of wrapping it around the code that code the logic that was likely to throw an error.

--- driver.py
+++ driver.py
@@ -5,30 +5,50 @@ import logging
 import sys
 import os
 
-logpath = os.path.dirname(__file__) + "/miner.log"
-logging.basicConfig(filename=logpath, filemode='a', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.info('Script Executed')
+# Logging 
+HOME_PATH = os.path.dirname(__file__)
+logging.basicConfig(filename=HOME_PATH + "/miner.log", filemode='a', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.info('Script Executed from {}'.format(HOME_PATH))
 
-hash_status = ''
-efficiency_status = ''
-error = False
+# Debugging
+DEBUG = 0
+SAMPLE_DATA = [{'node': {'workerName': '01x1', 'details1H': {'hashrate': '0', 'status': 'Active', 'efficiency': '0', 'validShares': '1114', 'staleShares': '0', 'badShares': '0', 'duplicateShares': '0', 'invalidShares': '8', 'lowDiffShares': '0'}}}, {'node': {'workerName': '01x02', 'details1H': {'hashrate': '95059532864261.6889', 'status': 'Active', 'efficiency': '0', 'validShares': '1564', 'staleShares': '0', 'badShares': '0', 'duplicateShares': '0', 'invalidShares': '0', 'lowDiffShares': '0'}}}, {'node': {'workerName': '01x03', 'details1H': {'hashrate': None, 'status': 'Dead', 'efficiency': None, 'validShares': None, 'staleShares': None, 'badShares': None, 'duplicateShares': None, 'invalidShares': None, 'lowDiffShares': None}}}, {'node': {'workerName': '01x04', 'details1H': {'hashrate': '0', 'status': 'Active', 'efficiency': '99.9312', 'validShares': '1453', 'staleShares': '0', 'badShares': '0', 'duplicateShares': '0', 'invalidShares': '1', 'lowDiffShares': '0'}}}, {'node': {'workerName': '01x05', 'details1H': {'hashrate': '95417632920844.5156', 'status': 'Active', 'efficiency': '99.5716', 'validShares': '1162', 'staleShares': '0', 'badShares': '0', 'duplicateShares': '0', 'invalidShares': '5', 'lowDiffShares': '0'}}}]
 
+# Select dataset to use
+if DEBUG == 0:
+    miner_data = worker_status.get_miner_data()
+else:
+    miner_data = SAMPLE_DATA
+
+# Check on miner status
+hash_status = worker_status.check_miner_efficiency(miner_data)
+efficiency_status = worker_status.check_miner_hashrate(miner_data)
+offline_status = worker_status.check_offline(miner_data)
+msg = hash_status + '\n' + efficiency_status + '\n' + offline_status
+
+# Notification
 try: 
-    hash_status = worker_status.check_miner_efficiency()
-    efficiency_status = worker_status.check_miner_hashrate()
-except Exception as ex: 
-    logging.exception("Error checking miner status.")
-    sys.exit()
-else: 
-    if len(hash_status) > 0 or len(efficiency_status) > 0:
-        error_msg = hash_status + "\n" + efficiency_status
-        error = True
+    f = open(HOME_PATH + "/flags.txt", "r")
+except Exception as ex:
+    logging.exception("Error opening flags file for reading")
 
-if error == True:
+line = f.readline()
+notified = line.split('=')[1].strip()
+f.close()
+
+if len(msg) > 0 and notified == 'False':
     try: 
-        push_notification.notify("Mining Alert", error_msg)
+        pass
+        push_notification.notify("Mining Alert", msg)
     except Exception as ex:
         logging.exception("Error sending push notification")
-    else:
-        logging.info("Miner performance is degraded.")
-        logging.error(error_msg)
+
+    try:
+        f = open(HOME_PATH + '/flags.txt', 'w')
+    except Exception as ex:
+        logging.exception("Error opening flags file for writing")
+
+    f.write('notified=True')
+    f.close()
+
+    
--- worker_status.py
+++ worker_status.py
@@ -3,50 +3,74 @@ from luxor import API
 import json
 import os
 import logging
+import sys
 
 ## SETUP ##
 API_KEY = os.getenv("LUX_API")
 API = API(host = "https://api.beta.luxor.tech/graphql", method = 'POST', org = 'luxor', key = API_KEY)
 USER = API.get_subaccounts(1)['data']['users']['edges'][0]['node']['username']
-DEBUG = 0
 EFFICIENCY_BASELINE = 98 # Measured as a percentage. ie 97% efficiency. 
 HASH_BASELINE = 90000000000000 # Measured in hashes per second 1 TH = 1 X 10^12 hashes per second 
 NUM_WORKERS = 6 # Number of workers in subaccount pool 
 
-# Used for Debugging
-SAMPLE_DATA = [{'node': {'workerName': '01x01', 'details1H': {'hashrate': '89000000000', 'status': 'Active', 'efficiency': '95.0000', 'validShares': '1475', 'staleShares': '0', 'badShares': '0', 'duplicateShares': '0', 'invalidShares': '0', 'lowDiffShares': '0'}}}]
 
 ## FUNCTIONS ##
-def check_miner_efficiency():
+def check_miner_efficiency(miner_dataset):
     msg = ''
-    json_miner_data = get_miner_data()
-    for miner in json_miner_data:
+    for miner in miner_dataset:
         name = miner['node']['workerName']
         efficiency = miner['node']['details1H']['efficiency']
-        if float(efficiency) < EFFICIENCY_BASELINE:
-            msg += 'Efficiency Issue: Miner {}, efficiency below threshhold --> {}'.format(name,efficiency)
+
+        if efficiency == None:
+            tmp_msg = '{}: did not report efficiency data. Offline?\n'.format(name)
+            logging.error(tmp_msg)
+            msg += tmp_msg
+
+        elif float(efficiency) < EFFICIENCY_BASELINE:
+            tmp_msg = '{}: efficiency of {} is below threshhold of {}.\n'.format(name,efficiency,EFFICIENCY_BASELINE)
+            logging.error(tmp_msg)
+            msg += tmp_msg
+
     return(msg)
 
-def check_miner_hashrate():
+def check_miner_hashrate(miner_dataset):
     msg = ''
-    json_miner_data = get_miner_data()
-    for miner in json_miner_data:
+    for miner in miner_dataset:
         name = miner['node']['workerName']
         hashrate = miner['node']['details1H']['hashrate']
-        if float(hashrate) < HASH_BASELINE:
-            msg='Hashrate Issue: Miner {}, hashrate below threshold --> {}\n'.format(name,hashrate)
+
+        if hashrate == None:
+            tmp_msg = "{}: did not report hashrate data. Offline?\n".format(name)
+            logging.error(tmp_msg)
+            msg += tmp_msg
+
+        elif float(hashrate) < HASH_BASELINE:
+            tmp_msg = '{}: hashrate of {} is below threshhold of {}.\n'.format(name,hashrate,HASH_BASELINE)
+            logging.error(tmp_msg)
+            msg += tmp_msg
+  
     return(msg)
 
-def pretty_json(json_data):
-    json_str = json.dumps(json_data, indent=2)
-    print(json_str)
+def check_offline(miner_dataset):
+    msg = ''
+    for miner in miner_dataset:
+        name = miner['node']['workerName']
+        status = miner['node']['details1H']['status']
+
+        if status == 'Dead':
+            tmp_msg = '{} is offline.\n'.format(name)
+            logging.error(tmp_msg)
+            msg += tmp_msg
+    return(msg)
 
 def get_miner_data(): 
-    resp = API.get_worker_details_1H(USER, "BTC", NUM_WORKERS)
+    try:
+        resp = API.get_worker_details_1H(USER, "BTC", NUM_WORKERS)
+    except Exception as ex:
+        logging.exception("Error trying to call luxor API with get_miner_data")
+        sys.exit()
 
-    if DEBUG == 1: 
-        json_miner_data = SAMPLE_DATA
-    else: 
-        json_miner_data = resp['data']['miners']['edges']
+    json_miner_data = resp['data']['miners']['edges']
+    logging.debug(json_miner_data)
 
     return(json_miner_data)
